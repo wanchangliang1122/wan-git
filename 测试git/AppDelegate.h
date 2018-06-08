f44897d5ac7438d755b29ee1b0c12dcc9d33ad40
D8Ge <burnedfrog@163.com>
2018-06-08 15:23:51 +0800

    Initial Commit

---
+++ 测试git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  测试git
+//
+//  Created by yeyongping on 2018/6/8.
+//  Copyright © 2018年 wanchangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 测试git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  测试git
+//
+//  Created by yeyongping on 2018/6/8.
+//  Copyright © 2018年 wanchangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
